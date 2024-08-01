882a6c3148daf8b9bf0a799190108ee9b54cf90f
SomaTheRipper <bh232332@gmail.com>
2024-08-01 07:53:49 +0700

    Add project files.

---
+++ SIMS_DEMO003/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SIMS_DEMO003.Models;
+using SIMS_DEMO003.Data;
+
+namespace SIMS_DEMO003.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+        public DbSet<SIMS_DEMO003.Models.Semesters> Semesters { get; set; } = default!;
+        public DbSet<SIMS_DEMO003.Models.Departments> Departments { get; set; } = default!;
+        public DbSet<SIMS_DEMO003.Models.Majors> Majors { get; set; } = default!;
+        
+        
+    }
+}
---
+++ SIMS_DEMO003/Models/Departments.cs
@@ -0,0 +1,9 @@
+﻿namespace SIMS_DEMO003.Models
+{
+    public class Departments
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+    }
+}
